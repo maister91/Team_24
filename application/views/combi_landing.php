a35ac6037ed34c9327abcde3fbd5e8e132cf2f6e
r0699038 <r0699038@student.thomasmore.be>
2019-03-28 13:38:06 +0100

    knoppen model_landing en combi landing

--- application/views/combi_landing.php
+++ application/views/combi_landing.php
@@ -1,14 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: simon
- * Date: 25/03/2019
- * Time: 22:49
- */
-?>
-
 <h1><?php echo $titel?></h1>
 
-<?php echo form_open('Traject/index'); ?>
-    <button type='submit' name='Kiestraject'>Verander traject</button>
-<?php echo form_close(); ?>
+<table class="table table-borderless ">
+    <tbody>
+    <tr>
+        <td>
+            <?php echo form_open('Traject/index'); ?>
+            <button type='submit' name='Terug' class="btn btn-primary">Terug</button>
+            <?php echo form_close(); ?>
+        </td>
+        <td>
+            <?php echo form_open('Traject/index'); ?>
+            <button type='submit' name='Simulatie' class="btn btn-primary">Simulatie</button>
+            <?php echo form_close(); ?>
+        </td>
+        <td>
+            <?php echo form_open('Gebruiker/meldAf'); ?>
+            <button type='submit' name='Afmelden' class="btn btn-primary">Afmelden</button>
+            <?php echo form_close(); ?>
+        </td>
+    </tr>
+    </tbody>
+    </table>
--- application/views/model_landing.php
+++ application/views/model_landing.php
@@ -1,18 +1,23 @@
 <h1><?php echo $titel?></h1>
 
-<div class="terug">
-    <?php echo form_open('Traject/index'); ?>
-    <button type='submit' name='Kiestraject' class="btn btn-primary">Terug</button>
-    <?php echo form_close(); ?>
-</div>
-<div class="klaskeuze">
-    <?php echo form_open('Traject/index'); ?>
-    <button type='submit' name='Klaskeuze' class="btn btn-primary">Klaskeuze maken</button>
-    <?php echo form_close(); ?>
-</div>
-<div class="afmelden">
-    <?php echo form_open('Gebruiker/meldAf'); ?>
-    <button type='submit' name='Afmelden' class="btn btn-primary">Afmelden</button>
-    <?php echo form_close(); ?>
-</div>
-
+<table class="table table-borderless ">
+    <tbody>
+    <tr>
+        <td>
+            <?php echo form_open('Traject/index'); ?>
+            <button type='submit' name='Terug' class="btn btn-primary">Terug</button>
+            <?php echo form_close(); ?>
+        </td>
+        <td>
+            <?php echo form_open('Traject/index'); ?>
+            <button type='submit' name='Klaskeuze' class="btn btn-primary">Klaskeuze maken</button>
+            <?php echo form_close(); ?>
+        </td>
+        <td>
+            <?php echo form_open('Gebruiker/meldAf'); ?>
+            <button type='submit' name='Afmelden' class="btn btn-primary">Afmelden</button>
+            <?php echo form_close(); ?>
+        </td>
+    </tr>
+    </tbody>
+    </table>
