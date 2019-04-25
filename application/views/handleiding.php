c10c8d44bcf3f859acaa06ddcc59fcfd9c51d4a5
melih <melih.91@hotmail.com>
2019-04-25 14:50:36 +0200

    lessenrooster

--- application/views/handleiding.php
+++ application/views/handleiding.php
@@ -8,3 +8,29 @@
 
 ?>
 <h1>Handleiding Excel Lessenrooster importeren</h1>
+<div class="container">
+    <p>De opleidingsmanager heeft de taak om in het begin van het schooljaar een lessenrooster te importeren</p>
+    <p>Een lessenrooster is een excel bestand en het is zeer belangrijk dat in dit bestand alle gegevens correct genoteerd zijn.</p>
+    <br> In deze handleiding gaan we stap voor stap laten zien hoe het moet en waar je op moet letten.
+</div>
+
+<h2>Stap 1</h2>
+<?php echo toonAfbeelding('images/Handleiding-url.JPG'); ?>
+<p>Zorg ervoor dat je op de correcte url bent.</p>
+<h2>Stap 2</h2>
+<?php echo toonAfbeelding('images/stap2.JPG'); ?>
+<p>Klik op bestand kiezen waarbij je een excel bestand kiest. </p>
+<h2>Stap 3</h2>
+<p>Voor het klikken op importeren moet je op volgende zaken binnen het excel bestand letten</p>
+<?php echo toonAfbeelding('images/stap3-a.JPG'); ?>
+<p>Een deel van het lessenrooster ziet er als volgt uit. Zorg ervoor dat deze structuur wordt behouden voor andere jaren</p>
+<?php echo toonAfbeelding('images/stap3-B.JPG'); ?>
+<p>Het bestand moet beginnen in rij 1 </p>
+<?php echo toonAfbeelding('images/stap3-c.JPG'); ?>
+<h2>Stap 4</h2>
+<?php echo toonAfbeelding('images/stap4.JPG'); ?>
+<p>Zorg ervoor dat nergens commentaar staat!!</p>
+<h2>Stap5</h2>
+<?php echo toonAfbeelding('images/stap2.JPG'); ?>
+<p>Nu mag je op import drukken</p>
+<?php echo toonAfbeelding('images/stap5.JPG'); ?>
