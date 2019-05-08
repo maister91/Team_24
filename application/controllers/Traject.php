996b6218bf3b8bce5f6059f4f72e0a9ae6fc5aa5
Melih Doksanbir <melih.91@hotmail.com>
2019-05-08 18:30:20 +0200

    dev

--- application/controllers/Traject.php
+++ application/controllers/Traject.php
@@ -1,112 +1,115 @@
 <?php
-/**
- * @property Template $template
- * @property Traject_model $traject_model
- * @property Gebruiker_model $gebruiker_model
- */
 
 class Traject extends CI_Controller
 {
+
+    /* @var Traject_model */
+    public $Traject_model;
+    /* @var Vak_model */
+    public $Vak_model;
+    /* @var Richting_model */
+    public $Richting_model;
+    /* @var Klas_model */
+    public $Klas_model;
+    /* @var Lesmoment_model */
+    public $Lesmoment_model;
+
     function __construct()
     {
         parent::__construct();
-        $this->load->model('traject_model');
-        $this->load->model('gebruiker_model');
+        $this->load->model('Traject_model');
+        $this->load->model('Vak_model');
+        $this->load->model('Richting_model');
+        $this->load->model('Klas_model');
+        $this->load->model('Lesmoment_model');
     }
+
     /*
      * Listing of traject
      */
     function index()
     {
-        $data['titel'] = '';
-        $data['ontwikkelaar'] = 'Simon Smedts';
-        $data['tester'] = 'Melih Doksanbir';
-        $data['trajecten'] = $this->traject_model->get_all_traject();
-        $partials = ['hoofding' => 'main_header',
-            'inhoud' => 'traject/index',
-            'voetnoot' => 'main_footer'];
-        $this->template->load('main_master', $partials, $data);
-    }
-    function kiesTraject()
-    {
-        $data['trajecten'] = $this->traject_model->get_all_traject();
         $gebruikerId = $this->authex->getGebruikerInfo()->id;
-        if ($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['knop'])) {
-            $knop = $this->input->post("knop");
-            if ($knop == "Model traject") {
-                $this->traject_model->update_traject(1, $gebruikerId);
-                $data['titel'] = 'Model student landing page';
-                $partials = ['hoofding' => 'main_header',
-                    'inhoud' => 'model_landing',
-                    'voetnoot' => 'main_footer'];
-                $this->template->load('main_master', $partials, $data);
-            }
-            else if ($knop == "Combi traject"){
-                $this->traject_model->update_traject(2, $gebruikerId);
-                $data['titel'] = 'Combi student landing page';
-                $partials = ['hoofding' => 'main_header',
-                    'inhoud' => 'combi_landing',
-                    'voetnoot' => 'main_footer'];
-                $this->template->load('main_master', $partials, $data);
-            }
-        }
+        $data['trajecten'] = $this->Traject_model->get_all_traject();
+        $data['_view'] = 'traject/index';
+        $this->load->view('layouts/main', $data);
     }
 
-    function keuzeTraject($id)
+    function combi()
     {
-        // check if the gebruiker exists before trying to edit it
-        $data['gebruiker'] = $this->gebruiker_model->get_gebruiker($id);
-        if(isset($data['gebruiker']['id']))
-        {
-            if(isset($_POST) && count($_POST) > 0)
-            {
-                $params = array(
-                    'gebruikertypeId' => $this->input->post('gebruikertypeId'),
-                    'trajectId' => $this->input->post('trajectId'))
-                ;
-                $this->gebruiker_model->update_gebruiker($id,$params);
-                redirect('traject_aanduiden');
-            }
-            else
-            {
-                $data['_view'] = 'gebruiker/edit';
-                $this->load->view('layouts/main',$data);
-            }
+        $klasId = $this->input->post('klassen');
+        $semesterId = $this->input->post('semester');
+
+        $gebruikerId = $this->authex->getGebruikerInfo()->id;
+        $data['feedback'] = '';
+        if ($this->input->get('klasId')) {
+            $this->Lesmoment_model->update_klas($this->input->get('klasId'), $gebruikerId);
+            $klasId = $this->input->get('klasId');
+            $semesterId = $this->input->get('semesterId');
+            $data['feedback'] = 'keuzeSuccesvol';
         }
-        else
-            show_error('The gebruiker you are trying to edit does not exist.');
+
+        $lesmomenten = $this->Lesmoment_model->get_lesmoment_by_klas_en_semester($klasId, $semesterId);
+        $rooster = [];
+        $vakkenUniek = [];
+        foreach ($lesmomenten as $lesmoment) {
+            $vak = $this->Vak_model->get_vak($lesmoment['vakId']);
+            if (!in_array($vak, $vakkenUniek)) {
+                $vakkenUniek[] = $vak;
+            }
+            $rooster[$lesmoment['lesblok']][$lesmoment['weekdag']] = [
+                'lesblok' => $lesmoment['lesblok'],
+                'vakId'   => $vak['id'],
+                'vakNaam' => $vak['naam'],
+            ];
+        }
+        $data['lesmomenten'] = $rooster;
+        $data['vakkenUniek'] = $vakkenUniek;
+        $data['klasId'] = $klasId;
+        $data['semesterId'] = $semesterId;
+        $data['klassen'] = $this->Klas_model->get_all_klassen();
+        $data['vakken'] = $this->Vak_model->get_all_vak();
+        $data['richtingen'] = $this->Richting_model->get_all_richting();
+        $data['_view'] = 'traject/combi';
+        $this->load->view('layouts/main', $data);
     }
+
+
     /*
      * Adding a new traject
      */
     function add()
     {
-        if (isset($_POST) && count($_POST)>0) {
+        if (isset($_POST) && count($_POST) > 0) {
             $params = [
-                'naam'         => $this->input->post('naam'),
+                'naam' => $this->input->post('naam'),
                 'beschrijving' => $this->input->post('beschrijving'),
             ];
-            $traject_id = $this->traject_model->add_traject($params);
+
+            $traject_id = $this->Traject_model->add_traject($params);
             redirect('traject/index');
         } else {
             $data['_view'] = 'traject/add';
             $this->load->view('layouts/main', $data);
         }
     }
+
     /*
      * Editing a traject
      */
     function edit($id)
     {
         // check if the traject exists before trying to edit it
-        $data['traject'] = $this->traject_model->get_traject($id);
+        $data['traject'] = $this->Traject_model->get_traject($id);
+
         if (isset($data['traject']['id'])) {
-            if (isset($_POST) && count($_POST)>0) {
+            if (isset($_POST) && count($_POST) > 0) {
                 $params = [
-                    'naam'         => $this->input->post('naam'),
+                    'naam' => $this->input->post('naam'),
                     'beschrijving' => $this->input->post('beschrijving'),
                 ];
-                $this->traject_model->update_traject($id, $params);
+
+                $this->Traject_model->update_traject($id, $params);
                 redirect('traject/index');
             } else {
                 $data['_view'] = 'traject/edit';
@@ -115,17 +118,83 @@ class Traject extends CI_Controller
         } else
             show_error('The traject you are trying to edit does not exist.');
     }
+
     /*
      * Deleting traject
      */
     function remove($id)
     {
-        $traject = $this->traject_model->get_traject($id);
+        $traject = $this->Traject_model->get_traject($id);
+
         // check if the traject exists before trying to delete it
         if (isset($traject['id'])) {
-            $this->traject_model->delete_traject($id);
+            $this->Traject_model->delete_traject($id);
             redirect('traject/index');
         } else
             show_error('The traject you are trying to delete does not exist.');
+    }
+
+    public function ajaxRequestPost()
+    {
+        $klasId = $this->input->post('klasId');
+        $semesterId = $this->input->post('semesterId');
+        $items = $this->input->post('items');
+
+        $lesmomenten = $this->Lesmoment_model->get_lesmoment_by_klas_en_semester($klasId, $semesterId);
+        $rooster = [];
+        foreach ($lesmomenten as $lesmoment) {
+            $vak = $this->Vak_model->get_vak($lesmoment['vakId']);
+            if (in_array($vak['id'], json_decode($items, true))) {
+                $rooster[$lesmoment['lesblok']][$lesmoment['weekdag']] = [
+                    'lesblok' => $lesmoment['lesblok'],
+                    'vakId'   => $vak['id'],
+                    'vakNaam' => $vak['naam'],
+                ];
+            }
+        }
+        $lesmomenten = $rooster;
+        ob_start();
+        ?><table class="table">
+            <thead>
+            <tr>
+                <th></th>
+                <th>Maandag</th>
+                <th>Dinsdag</th>
+                <th>Woensdag</th>
+                <th>Donderdag</th>
+                <th>Vrijdag</th>
+            </tr>
+            </thead>
+            <tbody><?php
+                for ($i = 1; $i <= 5; ++$i) {
+                    if (isset($lesmomenten[$i])) {
+                        $lesmoment = $lesmomenten[$i];
+                        ?>
+                        <tr>
+                        <td><?php echo $i; ?></td><?php
+                        for ($j = 0; $j <= 4; ++$j) {
+                            if (isset($lesmoment[$j])) {
+                                ?>
+                                <td><?php echo $lesmoment[$j]['vakNaam']; ?></td><?php
+                            } else {
+                                ?>
+                                <td></td><?php
+                            }
+                        }
+                    } else {
+                            ?>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr><?php
+                        }
+                    }
+                    ?></tbody>
+                </table><?php
+        $data = ob_get_clean();
+        echo $data;
     }
 }
---
+++ application/views/traject/combi.php
@@ -0,0 +1,109 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js"></script>
+
+<form method="post" accept-charset="utf-8" action="<?php echo site_url("traject/combi"); ?>">
+    <select name="klassen" onchange="this.form.submit()">
+        <option value="0">--- Maak een keuze ---</option><?php
+        foreach ($klassen as $klas) {
+            if ($klasId === $klas['id']) {
+                echo '<option value="' . $klas['id'] . '" selected>' . $klas['naam'] . '</option>';
+            } else {
+                echo '<option value="' . $klas['id'] . '">' . $klas['naam'] . '</option>';
+            }
+        }
+        ?></select>
+    <select name="semester" onchange="this.form.submit()">
+        ><?php
+        if ($semesterId == 2) {
+            echo '<option selected>2</option>';
+            echo '<option>1</option>';
+        } else {
+            echo '<option>2 </option>';
+            echo '<option selected>1 </option>';
+        }
+        ?> </select>
+        <select id="js_vakken" name="vakken" class="selectpicker" multiple data-selected-text-format="count > 3" class="width: 'auto'"><?php
+            foreach ($vakkenUniek as $vak) {
+                echo '<option value="' . $vak['id'] . '" selected>' . $vak['naam'] . '</option>';
+            }
+        ?></select>
+</form>
+<?php
+if (empty($vakkenUniek)) {
+    ?><div class="alert-warning">Geen lessen beschikbaar in de gemaakte selectie.</div><?php
+    return;
+}
+?>
+<div id="feedback"></div>
+<div id="lessenrooster">
+    <table class="table">
+        <thead>
+        <tr>
+            <th></th>
+            <th>Maandag</th>
+            <th>Dinsdag</th>
+            <th>Woensdag</th>
+            <th>Donderdag</th>
+            <th>Vrijdag</th>
+        </tr>
+        </thead>
+        <tbody><?php
+        for ($i = 1; $i <= 5; ++$i) {
+            if (isset($lesmomenten[$i])) {
+                $lesmoment = $lesmomenten[$i];
+                ?>
+                <tr>
+                <td><?php echo $i; ?></td><?php
+                for ($j = 0; $j <= 4; ++$j) {
+                    if (isset($lesmoment[$j])) {
+                        ?><td><?php echo $lesmoment[$j]['vakNaam']; ?></td><?php
+                    } else {
+                        ?><td></td><?php
+                    }
+                }
+            } else {
+                ?>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr><?php
+            }
+        }
+        ?></tbody>
+    </table>
+</div>
+<?php
+$alleVakIds = [];
+foreach ($vakkenUniek as $vakken) {
+    $alleVakIds[] = $vakken['id'];
+}
+?>
+<script type="text/javascript">
+    $(function() {
+        var vakken = JSON.stringify(<?php echo json_encode($alleVakIds); ?>);
+        $('select').selectpicker();
+        $('#js_vakken').change(function() {
+            $('#feedback').html('<p class="alert-info"><strong>Bezig met het ophalen van de aangepaste lessenrooster...</strong></p>');
+            $.ajax({
+                type: "POST",
+                url: "<?php echo site_url("traject/ajaxRequestPost"); ?>",
+                data: {items: JSON.stringify($(this).val()), klasId: '<?php echo $klasId ?>', semesterId: '<?php echo $semesterId ?>'},
+                success: function(data){
+                    $('#feedback').html('<p class="alert-success"><strong>Klaar!</strong></p>');
+                    $('#lessenrooster').html(data);
+                },
+                failure: function(errMsg) {
+                    console.log('fail');
+                    alert(errMsg);
+                }
+            });
+        });
+    });
+
+</script>
